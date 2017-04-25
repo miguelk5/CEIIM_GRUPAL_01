b606942849bd033f1f53203ca22bfc5fa5ea3b09
usuario-PC\usuario <jalpeher89@gmail.com>
2017-04-26 00:12:08 +0200

    update try

--- PRACTICA_TOWER_DEFENSE/Assets/TEST/Javier/Scripts/Cannon.cs
+++ PRACTICA_TOWER_DEFENSE/Assets/TEST/Javier/Scripts/Cannon.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour {
 
-    
+    public GameObject bullet;
     public Transform rotationAxe;
     Vector2 mousePosition;
     Vector2 direction;
+    Transform rotationAtShoot;
     // Use this for initialization
-
+    bool shoot;
     private void Awake()
     {
-        rotationAxe = rotationAxe.GetComponent<Transform>();
+        rotationAxe = rotationAxe.GetComponent<Transform>();       
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        mousePosition = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        direction = new Vector2(mousePosition.x - rotationAxe.position.x, mousePosition.y - rotationAxe.position.y);
-        rotationAxe.right = direction;   
+        Direction();
+        //Shoot();
 	}
+
+    void Direction()
+    {
+        mousePosition = (Camera.main.ScreenToWorldPoint(Input.mousePosition)); //toma la posición del ratón
+        direction = new Vector2(mousePosition.x - rotationAxe.position.x, mousePosition.y - rotationAxe.position.y); // vector direccion tomando como punto final la posición del ratón y como punto inicial la del eje de rotación
+        rotationAxe.right = direction;   //Hacer que el vector Right del eje de rotación tome la dirección calculada anteriormente
+    }
+
+    //void Shoot()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //     Instantiate(bullet,rotationAxe);           
+    //}
 }
