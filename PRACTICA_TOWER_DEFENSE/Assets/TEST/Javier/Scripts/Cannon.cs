efbfebb86cd71efb771a3b1d38adc706b892d6eb
usuario-PC\usuario <jalpeher89@gmail.com>
2017-04-25 22:43:23 +0200

    Cañon rotando

---
+++ PRACTICA_TOWER_DEFENSE/Assets/TEST/Javier/Scripts/Cannon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    
+    public Transform rotationAxe;
+    Vector2 mousePosition;
+    Vector2 direction;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        rotationAxe = rotationAxe.GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        mousePosition = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        direction = new Vector2(mousePosition.x - rotationAxe.position.x, mousePosition.y - rotationAxe.position.y);
+        rotationAxe.right = direction;   
+	}
+}
